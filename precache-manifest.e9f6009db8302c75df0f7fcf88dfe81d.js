9486591571bf169ea4c7fc17d190f4a45255a96a
bufu2o2 <bufu2o2@gmail.com>
2019-06-17 07:40:23 -0700

    Updates

---
+++ precache-manifest.e9f6009db8302c75df0f7fcf88dfe81d.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbe5a62d4e0a731d4996321b675f1957",
+    "url": "/clicky/index.html"
+  },
+  {
+    "revision": "0d7631c3b59183a3b3aa",
+    "url": "/clicky/static/css/main.d96a56e8.chunk.css"
+  },
+  {
+    "revision": "84938b74e7a7cfed7401",
+    "url": "/clicky/static/js/2.927b0ef0.chunk.js"
+  },
+  {
+    "revision": "0d7631c3b59183a3b3aa",
+    "url": "/clicky/static/js/main.1e520ca2.chunk.js"
+  },
+  {
+    "revision": "e4363465db862e89c37e",
+    "url": "/clicky/static/js/runtime~main.c6956f21.js"
+  },
+  {
+    "revision": "25e309404b737bf64e618472c3a8480a",
+    "url": "/clicky/static/media/bulbasaur.25e30940.png"
+  },
+  {
+    "revision": "18032727bf8f2983658e3ce53a605417",
+    "url": "/clicky/static/media/charmander.18032727.png"
+  },
+  {
+    "revision": "654826f09f55c0006c762238768232f3",
+    "url": "/clicky/static/media/crocadile.654826f0.png"
+  },
+  {
+    "revision": "e68954dc2932ec8262e56c3334799bfb",
+    "url": "/clicky/static/media/eevee.e68954dc.png"
+  },
+  {
+    "revision": "824cf908b3ba65a70b4086d4c7c0eebd",
+    "url": "/clicky/static/media/flareon.824cf908.png"
+  },
+  {
+    "revision": "4ceb37e744fb6e5282db4b13d1fd8921",
+    "url": "/clicky/static/media/gengar.4ceb37e7.png"
+  },
+  {
+    "revision": "dce766b16be44891e3e773a70f140c4d",
+    "url": "/clicky/static/media/jigglypuff.dce766b1.png"
+  },
+  {
+    "revision": "70eb8b25476eabff72a671964eb60871",
+    "url": "/clicky/static/media/jolteon.70eb8b25.png"
+  },
+  {
+    "revision": "2a2e05ac0e3937d8943be95944d0f663",
+    "url": "/clicky/static/media/pichu.2a2e05ac.png"
+  },
+  {
+    "revision": "76f027c1d8ea2170cedd976017330331",
+    "url": "/clicky/static/media/pikachu.76f027c1.png"
+  },
+  {
+    "revision": "9b400fcb611ee205d00d503d60acf69e",
+    "url": "/clicky/static/media/snorlax.9b400fcb.png"
+  },
+  {
+    "revision": "aef36912866ec8c57ff0fe5c1100e00a",
+    "url": "/clicky/static/media/squirtle.aef36912.png"
+  }
+]);
